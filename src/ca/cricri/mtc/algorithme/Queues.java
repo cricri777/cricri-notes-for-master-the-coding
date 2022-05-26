4992e3a9bd49a76f60e4652673d2fd9a5a00cb4e
cricri777 <christian.bruneau777@gmail.com>
2022-05-26 16:43:35 -0400

    ajout queue structure de données

---
+++ src/ca/cricri/mtc/algorithme/Queues.java
@@ -0,0 +1,69 @@
+package ca.cricri.mtc.algorithme;
+
+public class Queues {
+    private Node first;
+    private Node last;
+    private int length;
+
+    public Queues(){
+        this.first = null;
+        this.last = null;
+        this.length = 0;
+    }
+
+    public Node peek(){
+        return this.last;
+    }
+
+    public void enqueue(Node value){
+        if(length == 0){
+            first = value;
+            last = value;
+        } else {
+            this.last.setNext(value);
+            this.last = value;
+        }
+        length++;
+    }
+
+    public Node dequeue(){
+        Node res = null;
+        if(this.last == null) {
+            System.out.println("dequeue not possible, queue empty");
+        } else if(this.last == this.first) { // Todo : implement hashcode and equals
+            res = this.first;
+            this.first = null;
+            this.last = null;
+            this.length--;
+        } else {
+            res = this.first;
+            this.first = this.first.getNext();
+            length--;
+        }
+        return res;
+    }
+
+    @Override
+    public String toString(){
+        return "Queue : " + this.first;
+    }
+}
+
+class MainQueue{
+    public static void main(String[] args) {
+        Queues queue = new Queues();
+
+        queue.enqueue(new Node(12, null));
+        queue.enqueue(new Node(13, null));
+        queue.enqueue(new Node(14, null));
+        queue.enqueue(new Node(15, null));
+
+        System.out.println(queue);
+        queue.dequeue();
+        queue.dequeue();
+        queue.dequeue();
+        queue.dequeue();
+        queue.dequeue();
+        System.out.println(queue);
+    }
+}
