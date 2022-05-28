55d4ed85306c478e06a6dcf5739980423ddad39b
cricri777 <christian.bruneau@simpleetdonnees.com>
2022-05-28 13:28:38 -0400

    maj algo recursion

--- src/ca/cricri/mtc/algorithme/Recursion.java
+++ src/ca/cricri/mtc/algorithme/Recursion.java
@@ -1,8 +1,14 @@
 package ca.cricri.mtc.algorithme;
 
 /**
- * Ceci n'est pas un algo (juste des notes)
- * Une fonction qui se réfère à elle même dans son implémentation
+ * Pourquoi utiliser un fonction recursive au lieu d'iterative ?
+ * pour :
+ *  - DRY
+ * contre :
+ *  - Large Stack Memory
+ *  - i.e fibonacci recursion O(2^n) vs iteratif O(n)
+ *
+ * good application : Merge sort, Quick sort, Tree traversal, Graph traversal
  */
 public class Recursion {
 
@@ -19,38 +25,42 @@ public class Recursion {
     }
 
     public static long factorielRecursif(long number) {
-        if(number == 1){
+        if(number == 1) {
             return 1;
         }
         return number * factorielRecursif(number - 1);
     }
 
-    /**
-     * TODO
-     */
     public static long fiboncciIteratif(long n) {
-        return -1;
+        long premierNumero = 0;
+        long deuxNumero = 1;
+        int i = 1;
+        while(i < n){
+            long tmp = deuxNumero;
+            deuxNumero = premierNumero + deuxNumero;
+            premierNumero = tmp;
+            i++;
+        }
+        return deuxNumero;
     }
 
 
-    /**
-     * TODO
-     */
     public static long fibonacciRecursif(long n) {
-        return -1;
+        if(n < 2) {
+            return n;
+        }
+        return fibonacciRecursif(n-1) + fibonacciRecursif(n-2);
     }
+
+    public static String reverseString(String sentence){
+        if(sentence.equals("")) {
+            return "";
+        }
+        return reverseString(sentence.substring(1)) + sentence.charAt(0);
+    }
+
 
     public static void main(String[] args) {
-//        System.out.println(factorielRecursif(3));
-//        System.out.println(factorielRecursif(4));
-//        System.out.println(factorielRecursif(5));
-//        System.out.println(factorielRecursif(20));
-
-        System.out.println(fiboncciIteratif(6)); //output 8
-        System.out.println(fiboncciIteratif(7)); //output 13
-        System.out.println(fiboncciIteratif(8)); //output 21
-        System.out.println(fiboncciIteratif(9)); //output 34
-        System.out.println(fiboncciIteratif(10)); //output 55
-        System.out.println(fiboncciIteratif(17)); //output 1597
+        System.out.println(reverseString("hello cricri"));
     }
 }
